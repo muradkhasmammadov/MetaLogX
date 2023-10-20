af9cd757fc4bf1733a5bf64cf89ffdbc43305574
murad khasmammadov <murad.07mx@gmail.com>
2023-10-20 09:36:50 +0300

    Initial commit

---
+++ metalogs/views.py
@@ -0,0 +1,117 @@
+from django.shortcuts import render
+import pandas as pd
+from django.http import JsonResponse
+from django.http import FileResponse
+
+def home(request):
+    if request.method == 'POST':
+        return process_uploaded_file(request)
+
+    return render(request, 'metalogs/home.html')
+
+
+def metalogs(request):
+    if request.method == 'POST':
+        return metalogs(request)
+    
+    return render(request, 'metalogs/metalogs.html', get_initial_context())
+
+
+def process_uploaded_file(request):
+    uploaded_file = request.FILES.get('file')
+    num_mrs = 6
+    print(num_mrs)
+    if not uploaded_file:
+        return JsonResponse({'error': 'No file uploaded'}, status=400)
+
+    try:
+        log_csv = pd.read_csv(uploaded_file)
+        is_multiple_type = request.POST.get('analysis_option') == 'multiple'
+        missing_columns = get_missing_columns(log_csv, is_multiple_type, int(num_mrs))
+
+        
+        if missing_columns:
+            return JsonResponse({'error': f'Missing columns in log file: {", ".join(missing_columns)}'}, status=400)
+
+        chart_data = get_chart_data(log_csv)
+        chart_data2 = get_chart_data2(log_csv)
+        chart_data3 = get_chart_data3(log_csv)
+        return render(request, 'metalogs/metalogs.html', {
+            'missing_columns': None,
+            'chart_data': chart_data,
+            'chart_data2': chart_data2,
+            'chart_data3': chart_data3,
+        })
+
+    except pd.errors.EmptyDataError as e:
+        return JsonResponse({'error': 'The uploaded file is empty or in an unsupported format.'}, status=400)
+
+    except Exception as e:
+        return JsonResponse({'error': f'Error processing file: {str(e)}'}, status=400)
+
+def get_initial_context():
+    return {
+        'missing_columns': None,
+        'chart_data': None,
+        'chart_data2': {'labels': [], 'data': []},
+        'chart_data3': None,
+    }
+
+def get_missing_columns(log_csv, is_multiple_type, num_mrs):
+    all_columns = log_csv.columns
+
+    if is_multiple_type:
+        mr_columns = [
+            f"input_testData",
+            f"output_testInput",
+            f"output_MR",
+            f"MR_checker",
+        ]
+        required_columns = [f"{col}_MR{i}" for i in range(1, num_mrs + 1) for col in mr_columns]
+    else:
+        mr_checker_columns = [f"MR{i}_checker" for i in range(1, num_mrs + 1)]
+        required_columns = [
+            # "input_testData",
+            "output_testInput",
+        ] + [f"output_MR{i}" for i in range(1, num_mrs + 1)] + mr_checker_columns
+    print(required_columns)
+    return [col for col in required_columns if col not in log_csv.columns]
+
+def get_chart_data(log_csv):
+    rule_violations = log_csv.iloc[:, 8:].apply(lambda x: (x == 'Violated').sum())
+    labels = log_csv.columns[8:].tolist()
+    data = rule_violations.tolist()
+    return {'labels': labels, 'data': data}
+
+def get_chart_data2(log_csv):
+    checker_columns = ['MR1_checker', 'MR2_checker', 'MR3_checker', 'MR4_checker', 'MR5_checker', 'MR6_checker']
+    crashed_rows = log_csv[checker_columns].apply(lambda x: x[~x.isin(['Violated', 'Not-violated'])].count())
+    labels = checker_columns
+    data = crashed_rows.tolist()
+    return {'labels': labels, 'data': data}
+
+def get_chart_data3(log_csv):
+    chart_data3_list = []
+    required_columns = log_csv.columns[7:]
+    
+    for idx, column in enumerate(required_columns):
+        rule_name = column.replace("_checker", "")
+        violated_rows = log_csv[log_csv[column] == 'Violated']
+        output_column = f'output_{rule_name}'
+        
+        if output_column not in log_csv.columns:
+            continue  
+        cleaned_values = violated_rows[output_column].apply(pd.to_numeric, errors='coerce').dropna()
+
+        if not cleaned_values.empty:
+            chart_data3_list.append({
+                'label': f'{output_column} for {rule_name} Violations',
+                'data': cleaned_values.tolist(),
+                'backgroundColor': 'rgba(255, 99, 132, 0.2)',
+                'borderColor': 'rgba(255, 99, 132, 1)',
+                'borderWidth': 1,
+            })
+
+    labels = [item['label'] for item in chart_data3_list]
+    datasets = chart_data3_list
+    return {'labels': labels, 'datasets': datasets}
