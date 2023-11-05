62c77b8cef36e07647769055ff973ba600655606
murad khasmammadov <murad.07mx@gmail.com>
2023-11-05 12:22:39 +0200

    Automatically selection the number of mrs will be in the log file for multipe type log files implemented

--- metalogs/views.py
+++ metalogs/views.py
@@ -56,7 +56,7 @@ def process_uploaded_file(request, num_mrs, file_type):
         chart_data3 = get_chart_data3(log_csv)
         chart_data4 = get_chart_data4(log_csv)
         chart_data5 = get_chart_data5(log_csv)
-        chart_data6 = get_chart_data5(log_csv)
+        chart_data6 = get_chart_data6(log_csv)
         chart_data7 = get_chart_data7(log_csv)
         
         return {
@@ -75,7 +75,7 @@ def process_uploaded_file(request, num_mrs, file_type):
         return 'upload'
 
     except Exception as e:
-        messages.error(request, str(e))
+        messages.error(request, 'Something went wrong check your file and try again!')
         return 'upload'
 
 
